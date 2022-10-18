5cf2126c0f0ca79da98b947b7663cff0f3afa970
Tony <tony@lookingpet.com>
2022-10-18 12:02:52 +0800

    added  doubleTap zoom-in / out

--- lib/src/easy_image_view.dart
+++ lib/src/easy_image_view.dart
@@ -29,26 +29,52 @@ class EasyImageView extends StatefulWidget {
 }
 
 class _EasyImageViewState extends State<EasyImageView> {
-  final TransformationController _transformationController =
-      TransformationController();
+  final TransformationController _transformationController = TransformationController();
+
+  TapDownDetails _doubleTapDetails=TapDownDetails();
 
   @override
   Widget build(BuildContext context) {
     return SizedBox.expand(
         key: const Key('easy_image_sized_box'),
-        child: InteractiveViewer(
+        child: 
+             InteractiveViewer(
           key: const Key('easy_image_interactive_viewer'),
           transformationController: _transformationController,
           minScale: widget.minScale,
           maxScale: widget.maxScale,
-          child: Image(image: widget.imageProvider),
+          child: GestureDetector(
+              onDoubleTapDown: _handleDoubleTapDown,
+              onDoubleTap: _handleDoubleTap,
+            child: Image(image: widget.imageProvider)
+          ),
           onInteractionEnd: (scaleEndDetails) {
             double scale = _transformationController.value.getMaxScaleOnAxis();
-
+             
             if (widget.onScaleChanged != null) {
               widget.onScaleChanged!(scale);
             }
           },
         ));
+  }
+
+  void _handleDoubleTapDown(TapDownDetails details) {
+    _doubleTapDetails = details;
+  }
+
+  void _handleDoubleTap() {
+    if (_transformationController.value != Matrix4.identity()) {
+      _transformationController.value = Matrix4.identity();
+    } else {
+      final position = _doubleTapDetails.localPosition;
+     
+      _transformationController.value = Matrix4.identity()
+      // Fox a 2x zoom
+      ..translate(-position.dx, -position.dy)
+      ..scale(2.0);
+      // For a 3x zoom
+        // ..translate(-position.dx * 2, -position.dy * 2)
+        // ..scale(3.0);
+    }
   }
 }
